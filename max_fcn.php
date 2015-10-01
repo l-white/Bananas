8281c152dcf749a825b59f6237810363fc97bf5b
Laura <law7676@outlook.com>
2015-10-01 17:09:18 -0500

    code tested through all iterations

--- max_fcn.php
+++ max_fcn.php
@@ -13,9 +13,20 @@ $num5;
 
 function find_the_max($num1, $num2, $num3, $num4, $num5) {
     if ($num1 > $num2 && $num1 > $num3 && $num1 > $num4 && $num1 > $num5) {
-        echo "$num1 is the largest number.";
+      echo "$num1 is the largest number.";
   }
+    if ($num2 > $num1 && $num2 > $num3 && $num2 > $num4 && $num2 > $num5) {
+      echo "$num2 is the largest number.";
+    }
+    if ($num3 > $num1 && $num3 > $num2 && $num3 > $num4 && $num3 > $num5) {
+      echo "$num3 is the largest number.";
+    }
+    if ($num4 > $num1 && $num4 > $num2 && $num4 > $num3 && $num4 > $num5) {
+      echo "$num4 is the largest number.";
+    }
+    if ($num5 > $num1 && $num5 > $num2 && $num5 > $num3 && $num5 > $num4) {
+      echo "$num5 is the largest number.";
+    }
 }
-
-find_the_max(5, 4, 3, 2, 1);
+find_the_max(3, 4, 1, 2, 6);
 ?>
