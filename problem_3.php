932496ad82aa1efd90a87c574e84ccdb07e2bf51
Laura <law7676@outlook.com>
2015-10-03 07:35:58 -0500

    add combo move instructions

---
+++ problem_3.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "Up" . "\n";
+echo "Down" . "\n";
+echo "Left" . "\n";
+echo "Left" . "\n";
+echo "Right" . "\n";
+echo "Right" . "\n";
+echo "Down" . "\n";
+echo "Spin" . "\n";
+echo "Down" . "\n";
+echo "Up" . "\n";
+echo "Left" . "\n";
+echo "Right" . "\n";
+echo "Left" . "\n";
+echo "Down" . "\n";
+echo "Spin" . "\n";
+echo "Up" . "\n";
+echo "Down" . "\n";
+echo "Jump" . "\n";
+
+?>
