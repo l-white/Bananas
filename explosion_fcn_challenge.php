ec5102d5337ee8b7c772554253aaa37d6afebfdd
Laura <law7676@outlook.com>
2015-10-03 06:29:42 -0500

    add continued functionality

--- explosion_fcn_challenge.php
+++ explosion_fcn_challenge.php
@@ -1,12 +1,31 @@
 <?php
 
-$sentence = "Hello my name is Inigo Montoya";
+// Loop through the string.
+  // What's a string?  What's a loop?
+
+$string = "I am a short sentence but getting longer";
+
 $space = " ";
 
-function output($space, $sentence) {
-  for ($i = 0; $i <= strlen($sentence); $i++) {
-  print_r($sentence[$i]);
+for ($i = 0; $i<= $strlen; $i++) {
+  // What do you want me to do once I loop through the string?
+    //Take each word and assign it to an index.
+      //What's a word?
+        //Each word is preceded and followed by a space.
+          //What's a delimiter?
+  if ($space[$i] === $string[$i]) {
+    //If the space number is equal to the word number, assign an index to the word. If the space number is greater than the word number, index is equal to the space index - 1. If the space index is less than the word index, the indexed word is the space index + 1.
+      $word = [$i];
+  } elseif ($space[$i] > $string[$i]) {
+      $word = [$space[$i - 1]];
+  } else {
+      $word = [$space[$i + 1]];
   }
+  //Now that you've indexed each word, print out the array.
+    //How do I do that?
+print_r($word[$i]);
 }
-output(" ", "Hello my name is Indigo Montoya");
+
+
+
 ?>
