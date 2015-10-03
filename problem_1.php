d459cdcc6f7a81c5dcc948809499b09a4b63d9ad
Laura <law7676@outlook.com>
2015-10-03 06:58:40 -0500

    compute string length

---
+++ problem_1.php
@@ -0,0 +1,13 @@
+<?php
+
+//Please compute the length of this string.
+
+$InputString = "Please tell me how long this sentence is";
+
+//How do I do that?
+  //Use the strlen function.
+
+echo "This string is " . strlen($string) . " characters long";
+
+
+?>
